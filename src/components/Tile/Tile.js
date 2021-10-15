b9c142d6319c2f4ed434a79f7fed856398f24f98
Alexander Kessaris <ank352@nyu.edu>
2021-10-14 23:27:35 -0400

    renamed color variables

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,7 +1,7 @@
 import { React } from 'react';
 import './Tile.scss';
 
-const Tile = ({ color }) => {
+const Tile = ({ color = 'white' }) => {
   const classes = `Tile color-${color}`;
   return <div className={classes}></div>
 };
